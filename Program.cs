8fabe6f28f25541c5428f238e3d65f3714615845
Warren Tran <warrentran2015@hotmail.com>
2022-05-31 14:57:42 -0400

    added order step

--- Program.cs
+++ Program.cs
@@ -11,8 +11,9 @@ namespace Variables
             char firstLetter = 'W';
             bool lovesSwimming = true;
             double height = 5.6;
+            decimal width = 5.32m;
 
-            Console.WriteLine("{0} is {1} years old and is {2} tall. The first letter in his name is a {3}. Does Warren love swimming? The answer to that is {4}.", name, age, height, firstLetter, lovesSwimming);
+            Console.WriteLine("{0} is {1} years old and is {2} tall. His width is {5}.The first letter in his name is a {3}. Does Warren love swimming? The answer to that is {4}." , name, age, height, firstLetter, lovesSwimming, width);
         }
     }
 }
