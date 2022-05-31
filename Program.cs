93af7a7c41df8720d0667f904d0e6050453e8e64
Warren Tran <warrentran2015@hotmail.com>
2022-05-31 14:36:36 -0400

    descriptive message

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Warren";
+            int age = 14;
+            char firstLetter = 'W';
+            bool lovesSwimming = true;
+            double height = 5.6;
+
+            Console.WriteLine("{0} is {1} years old and is {2} tall. The first letter in his name is a {3}. Does Warren love swimming? The answer to that is {4}.", name, age, height, firstLetter, lovesSwimming);
+        }
+    }
+}
